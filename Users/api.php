78cb058a3efd8dc7e1de3843f7697e2b11e28dd5
jodhi <jodhilesmana@gmail.com>
2017-05-08 15:49:30 +0000

    finish connect

--- Users/api.php
+++ Users/api.php
@@ -1,26 +1,34 @@
 <?php
-    //http://stackoverflow.com/questions/18382740/cors-not-working-php
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
+    // if (isset($_SERVER['HTTP_ORIGIN'])) {
+    //     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    //     header('Access-Control-Allow-Credentials: true');
+    //     header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    // }
+    //
+    // //http://stackoverflow.com/questions/15485354/angular-http-post-to-php-and-undefined
+    // $postdata = file_get_contents("php://input");
+    // if (isset($postdata)) {
+    //     $request = json_decode($postdata);
+    //     $username = $request->username;
+    //     $password = $request->password;
+    //     if ($username != "") {
+    //         echo "username is: " . $username;
+    //         echo "<br>";
+    //         echo "password is: " . $password;
+    //     } else {
+    //         echo "Empty username parameter!";
+    //     }
+    // } else {
+    //     echo "Not called properly with username parameter!";
+    // }
 
+    $json_input_data=json_decode(file_get_contents('php://input'), true);
 
-
-    //http://stackoverflow.com/questions/15485354/angular-http-post-to-php-and-undefined
-    $postdata = file_get_contents("php://input");
-    if (isset($postdata)) {
-        $request = json_decode($postdata);
-        $username = $request->username;
-        $password = $request->password;
-        if ($username != "") {
-            echo "username is: " . $username;
-            echo "<br>";
-            echo "password is: " . $password;
-        } else {
-            echo "Empty username parameter!";
-        }
+    $usrn=$json_input_data["username"];
+    $pass=$json_input_data["password"];
+    if ($usrn!="") {
+        echo "usrname is ".$usrn;
+        echo "pass is ".$pass;
     } else {
-        echo "Not called properly with username parameter!";
+        echo "empty username";
     }
